fe0968ce1f6520ea7dde87d8fbec5acacc5f404b
Raghu Masanam <raghavendraa779@gmail.com>
2026-08-17 15:24:10 +0530

    Add Java program for Jenkins CI

---
+++ HelloJenkins.java
@@ -0,0 +1,6 @@
+public class HelloJenkins {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins!");
+        System.out.println("Java code successfully compiled and executed.");
+    }
+}
